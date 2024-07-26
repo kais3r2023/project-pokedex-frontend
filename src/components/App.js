aa0c70396bcbb93d5c0e81474b1c1cd119e00631
kais3r2023 <122757568+kais3r2023@users.noreply.github.com>
2024-07-26 00:39:16 -0500

    Creacion de la solicitud para atrapar los datos de la Pokeapi

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Main from "./Main";
-import *as api from '../utils/api';
-
-
-
+import * as api from "../utils/api";
 
 function App() {
-
-  const [allPokemon, setAllPokemon] = useState([])
+  const [allPokemon, setAllPokemon] = useState([]);
+  const [pokemonStats, setPokemonStats] = useState({});
+  
 
   //Funcion para obtener Listado de Pokémon
+  useEffect(() => {
+    api.getAllPokemon().then((res) => {
+      setAllPokemon(res);
+    });
+  }, []);
+  
+  console.log(allPokemon)
 
-  useEffect(()=>{
-    api.getAllPokemon()
-        .then((res)=>{
-          setAllPokemon(res.pokemon_species)
-          console.log(res.pokemon_species)
-        })
-  },[])
+  //Funcion para guardar stats de Pokémon
+
 
   return (
     <div>
-      <Main/>
+      <Main />
     </div>
   );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,33 @@
-const URL = 'https://pokeapi.co/api/v2/generation/1/'
+const URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
-export const getAllPokemon = async() =>{
-  let pokemonList = []
-  try{
-    const apiResponse = await fetch(URL)
-    pokemonList = apiResponse.json()
-    return (pokemonList)
-  }
-  catch(error){
-    console.log('Error del servidor:', error);
+export const getAllPokemon = async () => {
+  try {
+    const data = await fetch(URL);
+    const allPokemon = await data.json();
+
+    const pokemonData = await Promise.all(
+      allPokemon.results.map(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const data = await response.json();
+        return {
+          id: data.id,
+          name: data.name,
+          image: data.sprites.other.dream_world.front_default,
+          cry: data.cries.legacy,
+          stats: {
+            HP: data.stats[0].base_stat,
+            Attack: data.stats[1].base_stat,
+            Defense: data.stats[2].base_stat,
+            SpecialAtk: data.stats[3].base_stat,
+            SpecialDef: data.stats[4].base_stat,
+            Speed: data.stats[5].base_stat,
+          },
+        };
+      })
+    );
+    return pokemonData;
+  } catch (error) {
+    console.log("Error del servidor:", error);
     throw error;
   }
-}
+};
